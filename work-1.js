7d3e905179ff712a8d7b17868020357d4903df44
송이현 <songehyun00@gmail.com>
2024-07-04 09:26:18 +0900

    타스 테스트 완료

---
+++ work-1.js
@@ -0,0 +1,10 @@
+function a(b) {
+  if (typeof b === "number") {
+    if (Number.isInteger(b) === true) {
+      let result = b + 10;
+      return result;
+    }
+  }
+}
+
+console.log(a("조우식"));
---
+++ work-2.ts
@@ -0,0 +1,6 @@
+type name = "조우식";
+
+const a = 1;
+let b: name = "조우식";
+
+console.log(b);
